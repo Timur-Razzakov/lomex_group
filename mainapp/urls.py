090f998fedd524fbdd5313a314aa334676ae6840
Timur-Razzakov <razzakov334@gmail.com>
2021-11-11 08:14:57 +0500

    added a few changesЭ

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('api/genres/', views.get_api_genres, name='genres'),
-    path('api/actors/', views.get_api_actors, name='actors'),
-    path('api/directors/', views.get_api_directors, name='directors'),
+    path('mainapp/genres/', views.get_api_genres, name='genres'),
+    path('mainapp/actors/', views.get_api_actors, name='actors'),
+    path('mainapp/directors/', views.get_api_directors, name='directors'),
 
 ]
